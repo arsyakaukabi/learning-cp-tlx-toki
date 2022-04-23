ded3464a9079f18a0c405ce0a8d785d541a6c46f
arsyakaukabi <arsyakaukabi@gmail.com>
2022-04-23 19:03:37 +0700

    Added new solutions

--- TitikMerah.py
+++ TitikMerah.py
@@ -1,3 +1,3 @@
-I=lambda:list(map(int,input().split()))
-n,r,v=I();w=[abs(i-v)for i in I()]
-print(-1 if min(w)==0 else r/min(w))
+I=lambda:map(int,input().split())
+n,r,v=I();w=min([abs(i-v)for i in I()])
+print(-1 if w==0 else r/w)
